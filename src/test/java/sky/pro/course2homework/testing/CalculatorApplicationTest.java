00e2ad00da0faf9389d85badabd770de259127e4
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-28 15:07:00 +0200

    2 Курс ДЗ № 25 Тестирование

---
+++ src/test/java/sky/pro/course2homework/testing/CalculatorApplicationTest.java
@@ -0,0 +1,17 @@
+package sky.pro.course2homework.testing;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class CalculatorApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
