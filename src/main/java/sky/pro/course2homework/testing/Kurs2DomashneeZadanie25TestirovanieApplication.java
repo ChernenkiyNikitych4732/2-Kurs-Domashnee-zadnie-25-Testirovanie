489f571acadc2a3b70c3e2fc2705e691652d437d
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-28 14:40:17 +0200

    2 Курс ДЗ № 25 Тестирование

---
+++ src/main/java/sky/pro/course2homework/testing/Kurs2DomashneeZadanie25TestirovanieApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homework.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2DomashneeZadanie25TestirovanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2DomashneeZadanie25TestirovanieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2homework/testing/Kurs2DomashneeZadanie25TestirovanieApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homework.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2DomashneeZadanie25TestirovanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
